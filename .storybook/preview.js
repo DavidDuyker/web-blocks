180ccaf0bb7fc5cc7415c84243bf33ad5336efe1
David Duyker <dduyker@gmail.com>
2025-10-06 09:13:32 -0700

    added link component to storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -22,7 +22,7 @@ export default {
     },
     options: {
       storySort: {
-        order: ['Introduction', 'Button', 'Input'],
+        order: ['Introduction', 'Link', 'Button', 'Input'],
       },
     },
   },
